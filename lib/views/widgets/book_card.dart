06d11c498934bc2ca81b74aa2b1361ba03f3ad95
BranislavMateas <mateas.branislav@gmail.com>
2022-01-21 14:24:44 +0100

    Card design improved

--- lib/views/widgets/book_card.dart
+++ lib/views/widgets/book_card.dart
@@ -18,23 +18,74 @@ class BookCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 200,
-        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        width: (MediaQuery.of(context).size.width),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(this.image),
-            alignment: Alignment.centerLeft,
-          ),
-          color: Colors.grey,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
-            bottomLeft: Radius.circular(20.0),
-            bottomRight: Radius.circular(20.0),
-          ),
+      height: 150,
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      width: (MediaQuery.of(context).size.width),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(this.image),
+          alignment: const Alignment(-1.1, 0.00),
         ),
-        child: Stack(children: [
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(10.0),
+          topRight: Radius.circular(10.0),
+          bottomLeft: Radius.circular(10.0),
+          bottomRight: Radius.circular(10.0),
+        ),
+        border: Border.all(
+          color: const Color(0xff888888),
+          width: 2.0,
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: FractionallySizedBox(
+          alignment: const Alignment(0.85, 0.0),
+          widthFactor: 0.675,
+          heightFactor: 0.88,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(this.title,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: Text(this.subtitle,
+                          style: const TextStyle(
+                            fontSize: 14,
+                          )),
+                    ),
+                  ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("ISBN: " + this.isbn13,
+                      style: const TextStyle(
+                        fontSize: 12,
+                      )),
+                  Text(this.price,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
+
+/*
+child: Stack(children: [
           Align(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 5.0),
@@ -49,6 +100,5 @@ class BookCard extends StatelessWidget {
             ),
             alignment: Alignment.centerRight,
           ),
-        ]));
-  }
-}
+        ]
+ */
